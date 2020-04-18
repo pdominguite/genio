3e713ee50ebe03a84470266c8a73b0436c687a98
Pedro Felipe Dominguite <pfdominguite@gmail.com>
2020-04-18 15:05:36 -0300

    Got request parameters

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const alexa = require('ask-sdk');
 
 const app = express();
 
-const port = process.env.PORT || 3333;
+const port = process.env.PORT || 3334;
 
 app.use(express.json());
+
+const LaunchRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest';
+  },
+  handle(handlerInput) {
+    const speechText = 'Luz acesa!';
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .reprompt(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  },
+};
 
 app.get('/', (req, res) => {
   return res.status(200).send("OK");
@@ -19,9 +34,8 @@ app.get('/genio/index', (req, res) => {
 })
 
 app.post('/genio', (req, res) => {
-  console.log(req.body.request.intent);
-  console.log(req.headers);
-  return res.status(204).json();
+  //console.log(req.body.request.intent);
+  return LaunchRequestHandler;
 })
 
 app.listen(port, () => {
